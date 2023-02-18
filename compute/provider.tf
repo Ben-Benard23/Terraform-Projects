71d77207553b2a31e65b17455a804e3bb49f726c
Anselme Numbisia <anselmenumbisia@gmail.com>
2023-02-18 16:00:56 -0700

    Modified terraform files

---
+++ compute/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+ # required_version = ">= 1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-west-2"
+
+}
+
+
+# Create a VPC
+resource "aws_vpc" "projectA-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+}
